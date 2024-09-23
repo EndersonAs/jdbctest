c5a430436b796528b7b7a8da8bdddae6b2ea1eab
EndersonAs <ariza.ambiente@gmail.com>
2024-09-23 10:32:06 -0500

    Proyecto jdbctest con las coxiones correspondientes

---
+++ src/main/java/com/mycompany/jdbctest/Jdbctest.java
@@ -0,0 +1,44 @@
+package com.mycompany.jdbctest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+ // @author ariza
+ 
+public class Jdbctest {
+
+    public static void main(String[] args) {
+        String usuario="root";
+        String password="";
+        String url="jdbc:mysql://localhost:3306/jdbctest";
+        
+            Connection conexion;
+            Statement statement;
+            ResultSet rs; 
+            
+        try {  
+            conexion = DriverManager.getConnection(url, usuario, password);
+            statement = conexion.createStatement();
+            rs = statement.executeQuery("SELECT * FROM usuarios");
+            while(rs.next()){
+                System.out.println(rs.getString("nombre"));  
+           }
+            // insertar Datos
+          /*  statement.execute("INSERT INTO `usuarios` (`Id`, `nombre`) VALUES (NULL, 'Eserna'); ");
+            System.out.println(""); // lo dejamos vacio para un salto de linea 
+            rs = statement.executeQuery("SELEC * FROM usuarios");
+            while(rs.next()){
+                System.out.println(rs.getString("nombre"));  
+              }*/
+          
+        } catch (SQLException ex) {
+            Logger.getLogger(Jdbctest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+
